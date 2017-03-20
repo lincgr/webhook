a4668b244d7c293d0b25dad5e45e388ce537eaef
lincgr <lincgr@gmail.com>
2017-03-20 16:17:30 -0600

    Update Webhook

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 'use strict';
-const express = require('express');
-const bodyParser = require('body-parser');
-var time = require('time');
+        const express = require('express');
+        const bodyParser = require('body-parser');
+        var time = require('time');
 const restService = express();
-restService.use(bodyParser.json());
+        restService.use(bodyParser.json());
 
 restService.post('/hook', function (req, res) {
 
@@ -20,15 +20,15 @@ restService.post('/hook', function (req, res) {
             if (requestBody.result) {
 
                 speech = '';
-                if(action.indexOf(requestBody.result.action)>=0) {
-                    if (requestBody.result.fulfillment){
-                        speech += requestBody.result.fulfillment.speech;
-                    }
-                    if (hour >= 7 && hour <= 19){
+                if (requestBody.result.fulfillment) {
+                    speech += requestBody.result.fulfillment.speech;
+                }
+                if (action.indexOf(requestBody.result.action) >= 0) {
+                    if (hour >= 7 && hour <= 19) {
                         speech += ' Please  write LegalShield Member Services at <a href=“http://home-c4.incontact.com/inContact/ChatClient/ChatClient.aspx?poc=785ac45b-4579-4198-9376-359d21b87f27&bu=4595114”> Chat Cient </a> . If we can be of any further assistance, please don’t hesitate to ask.';
                     } else {
                         speech += ' Please  write LegalShield Member Services at memberservices@legalshield.com. If we can be of any further assistance, please don’t hesitate to ask.';
-                   }
+                    }
                 }
             }
         }
