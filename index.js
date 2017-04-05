92325e0dc20a3f16dcca03f6748de813578ba0f7
lincgr <lincgr@gmail.com>
2017-04-05 16:11:31 -0600

    Connect with facebook

--- index.js
+++ index.js
@@ -15,8 +15,9 @@ restService.post('/hook', function (req, res) {
         var speech = 'empty speech';
 
         if (req.body) {
+            var data = req.body;
             var requestBody = req.body;
-            console.log(requestBody.result);
+            console.log(requestBody.result.contexts);
 
             if (requestBody.result) {
                 speech = '';
@@ -25,13 +26,25 @@ restService.post('/hook', function (req, res) {
                     speech += requestBody.result.fulfillment.speech;
                     speech += ' ';
                 }
-
-                if (requestBody.result.action) {
-                    speech += 'action: ' + requestBody.result.action;
-                }
             }
-        }
-        console.log('result: ', speech);
+
+             if (data.object === 'page') {
+                // Iterate over each entry - there may be multiple if batched
+                data.entry.forEach(function(entry) {
+                var pageID = entry.id;
+                var timeOfEvent = entry.time;
+
+                // Iterate over each messaging event
+                entry.messaging.forEach(function(event) {
+                    if (event.message) {
+                    receivedMessage(event);
+                    } else {
+                    console.log("Webhook received unknown event: ", event);
+                    }
+                });
+                });
+             }
+        }        
         return res.json({
             speech: speech,
             displayText: speech,
@@ -52,3 +65,35 @@ restService.post('/hook', function (req, res) {
 restService.listen((process.env.PORT || 5000), function () {
     console.log("Server listening");
 });
+
+function receivedMessage(event) {
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var timeOfMessage = event.timestamp;
+  var message = event.message;
+
+  console.log("Received message for user %d and page %d at %d with message:", 
+    senderID, recipientID, timeOfMessage);
+  console.log(JSON.stringify(message));
+
+  var messageId = message.mid;
+
+  var messageText = message.text;
+  var messageAttachments = message.attachments;
+
+  if (messageText) {
+
+    // If we receive a text message, check to see if it matches a keyword
+    // and send back the example. Otherwise, just echo the text we received.
+    switch (messageText) {
+      case 'generic':
+        sendGenericMessage(senderID);
+        break;
+
+      default:
+        sendTextMessage(senderID, messageText);
+    }
+  } else if (messageAttachments) {
+    sendTextMessage(senderID, "Message with attachment received");
+  }
+}
