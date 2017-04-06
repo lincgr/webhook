b1ee2bd2437a896822a7c415a5dbf6d52fa2218d
lincgr <lincgr@gmail.com>
2017-04-06 10:48:17 -0600

    Print

--- index.js
+++ index.js
@@ -28,23 +28,23 @@ restService.post('/hook', function (req, res) {
                 }
             }
 
-             if (data.object === 'page') {
+            if (data.object === 'page') {
                 // Iterate over each entry - there may be multiple if batched
-                data.entry.forEach(function(entry) {
-                var pageID = entry.id;
-                var timeOfEvent = entry.time;
+                data.entry.forEach(function (entry) {
+                    var pageID = entry.id;
+                    var timeOfEvent = entry.time;
 
-                // Iterate over each messaging event
-                entry.messaging.forEach(function(event) {
-                    if (event.message) {
-                    receivedMessage(event);
-                    } else {
-                    console.log("Webhook received unknown event: ", event);
-                    }
+                    // Iterate over each messaging event
+                    entry.messaging.forEach(function (event) {
+                        if (event.message) {
+                            receivedMessage(event);
+                        } else {
+                            console.log("Webhook received unknown event: ", event);
+                        }
+                    });
                 });
-                });
-             }
-        }        
+            }
+        }
         return res.json({
             speech: speech,
             displayText: speech,
@@ -67,33 +67,33 @@ restService.listen((process.env.PORT || 5000), function () {
 });
 
 function receivedMessage(event) {
-  var senderID = event.sender.id;
-  var recipientID = event.recipient.id;
-  var timeOfMessage = event.timestamp;
-  var message = event.message;
+    var senderID = event.sender.id;
+    var recipientID = event.recipient.id;
+    var timeOfMessage = event.timestamp;
+    var message = event.message;
 
-  console.log("Received message for user %d and page %d at %d with message:", 
-    senderID, recipientID, timeOfMessage);
-  console.log(JSON.stringify(message));
+    console.log("Received message for user %d and page %d at %d with message:",
+        senderID, recipientID, timeOfMessage);
+    console.log(JSON.stringify(message));
 
-  var messageId = message.mid;
+    var messageId = message.mid;
 
-  var messageText = message.text;
-  var messageAttachments = message.attachments;
+    var messageText = message.text;
+    var messageAttachments = message.attachments;
 
-  if (messageText) {
+    if (messageText) {
 
-    // If we receive a text message, check to see if it matches a keyword
-    // and send back the example. Otherwise, just echo the text we received.
-    switch (messageText) {
-      case 'generic':
-        sendGenericMessage(senderID);
-        break;
+        // If we receive a text message, check to see if it matches a keyword
+        // and send back the example. Otherwise, just echo the text we received.
+        switch (messageText) {
+            case 'generic':
+                sendGenericMessage(senderID);
+                break;
 
-      default:
-        sendTextMessage(senderID, messageText);
+            default:
+                sendTextMessage(senderID, messageText);
+        }
+    } else if (messageAttachments) {
+        sendTextMessage(senderID, "Message with attachment received");
     }
-  } else if (messageAttachments) {
-    sendTextMessage(senderID, "Message with attachment received");
-  }
 }
